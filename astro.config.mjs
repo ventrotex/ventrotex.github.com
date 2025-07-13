9b08d1d220459425d018a778640938dcd752ef2b
ventrotex <ventrotex@gmail.com>
2025-07-13 09:01:09 +0000

    Initial build

---
+++ astro.config.mjs
@@ -0,0 +1,9 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+
+export default defineConfig({
+	site: 'https://ventrotex.github.io',
+	integrations: [mdx(), sitemap()],
+});
